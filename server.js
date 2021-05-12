86cfdd622ddc14dcd7846ad71bacf0f1dff06158
Aleksandra Dahlin <Aleksandra.Dahlin@ikano.se>
2021-05-12 18:41:17 +0200

    cleanup

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-
 const connectDB = require("./config/db");
 const route = require("./routes");
 const establishSession = require("./services/session");
+
 const app = express();
-// const getUsers = require("./routes/register");
+
 connectDB();
+
 establishSession(app);
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to AJPay Server");
 });
+
 app.use(express.json());
 
-// getUsers(app);
-
-app.use("/api/test", require("./routes/test"));
 route(app);
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
